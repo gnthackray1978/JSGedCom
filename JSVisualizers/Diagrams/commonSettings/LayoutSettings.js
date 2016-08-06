e97b4e279f017bc5d1b5e6b012d2a7aac78a2041
George <gnthackray@btinternet.com>
2016-08-06 23:19:45 +0000

    refactoring exercise

---
+++ JSVisualizers/Diagrams/commonSettings/LayoutSettings.js
@@ -0,0 +1,14 @@
+
+
+var LayoutSettings = function (){
+
+    this.stiffness = 400.0;
+    this.repulsion = 500.0;
+    this.damping = 0.5;
+    this.colourScheme = colourScheme;
+    
+    this.speed =3000;
+    this.increment =5;
+    this.year = 1670;
+    
+};
